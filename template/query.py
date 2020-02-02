a38da790e5f669e686fc3a1b8db70de2d1f516d7
xu1998hz <wedxu@ucdavis.edu>
2020-02-01 22:49:13 -0800

    "update ?"

--- template/query.py
+++ template/query.py
@@ -1,13 +1,13 @@
 from template.table import Table, Record
 from template.index import Index
 from template.page import Page
-from template.config import * 
+from template.config import *
 from copy import copy
 import re
 
 class Query:
     """
-    # Creates a Query object that can perform different queries on the specified table 
+    # Creates a Query object that can perform different queries on the specified table
     """
 
     def __init__(self, table):
@@ -32,14 +32,14 @@ class Query:
         columns.extend([schema_encoding, MAXINT])
         for i, value in enumerate(columns):
             page = self.table.page_directory["Base"][i][-1]
-            
+
             # Verify Page is not full
             while not page.has_capacity():
                 self.table.page_directory["Base"][i].append(Page())
                 page = self.table.page_directory["Base"][i][-1]
-            
+
             page.write(value)
-        
+
     """
     # Read a record with specified key
     """
@@ -47,9 +47,9 @@ class Query:
     def select(self, key, query_columns):
         key_col_id = self.table.key # int
         pages = self.table.page_directory["Base"][key_col_id]
-        
+
         b_key = (key).to_bytes(8, byteorder='big')
-        
+
         page_id = 0
         rec_id = 0
         for i in range(len(pages)):
@@ -73,42 +73,56 @@ class Query:
 
     def update(self, key, *columns):
         key_col_id = self.table.key # int
-        pages = self.table.page_directory["Base"][key_col_id]
-        
-        b_key = (key).to_bytes(8, byteorder='big')
-        
-        indirection_id = MAXINT
-        for i in range(len(pages)):
-            for j in range(pages[i].num_records):
-                if (pages[i].get(j) == b_key):
-                    indirection_id = self.table.page_directory["Base"][-1].get(j)
+        key_pages = self.table.page_directory["Base"][key_col_id]
+        indirection_pages = self.table.page_directory["Base"][len(columns)+1]
+        update_record_index = 0
+        update_record_page_index = 0
 
+
+        b_key = (key).to_bytes(8, byteorder='big')
+
+        indirection_id = MAXINT
+        for i in range(len(key_pages)):
+            for j in range(key_pages[i].num_records):
+                if (key_pages[i].get(j) == b_key):
+                    update_record_index = j
+                    update_record_page_index = i
+                    break
+
+        indirection_id = self.table.page_directory["Base"][len(columns)+1][update_record_page_index].get(update_record_index)
+
+        int_indirection_id = int.from_bytes(indirection_id, byteorder="big")
+
+
+        list_columns = list(columns)
+        list_columns.extend([self.table.num_updates, int_indirection_id])
+        for col_id, col_val in enumerate(list_columns):
+            print(col_val)
+            if col_val == None:
+                col_val = MAXINT
+            # Create New Page if current tail of tail page if fulled
+            if not self.table.page_directory["Tail"][col_id][-1].has_capacity():
+                self.table.page_directory["Tail"][col_id].append(Page())
+
+            self.table.page_directory["Tail"][col_id][-1].write(col_val)
+
+        if int_indirection_id == MAXINT:
+            int_indirection_id = 0
+        else:
+            int_indirection_id += 1
+
+        self.table.page_directory["Base"][len(columns)+1][update_record_page_index].update(update_record_index, int_indirection_id)
         self.table.num_updates += 1
 
-        columns.extend([self.table.num_updates, indirection_id])
-        for col_id, col_val in enumerate(columns):
-            # Create New Page if current tail of tail page if fulled
-            if not self.page_directory["Tail"][col_id][-1].has_capacity():
-                self.page_directory["Tail"][col_id].append(Page())
-            
-            self.page_directory["Tail"][col_id][-1].write(col_val)
-
-        if indirection_id == MAXINT:
-            indirection_id = 0
-        else
-            indirection_id += 1
-
-        self.table.page_directory["Base"][-1].get(j) = indirection_id
-
-        
 
 
 
-        
+
+
 
     """
-    :param start_range: int         # Start of the key range to aggregate 
-    :param end_range: int           # End of the key range to aggregate 
+    :param start_range: int         # Start of the key range to aggregate
+    :param end_range: int           # End of the key range to aggregate
     :param aggregate_columns: int   # Index of desired column to aggregate
     """
 
--- test.py
+++ test.py
@@ -8,14 +8,14 @@ from template.db import Database
 class Query_Tester:
     def __init__(self):
         self.db = Database()
-        self.table = self.db.create_table("test", 6, 0)    
+        self.table = self.db.create_table("test", 6, 0)
 
     def test_insert(self):
         self.query = Query(self.table)
 
-        for i in range(513):
+        for i in range(514):
             self.query.insert(i, 1+i, 2+i, 3+i, 4+i, 5+i)
-        
+
         # for col_id in range(len(self.table.page_directory["Base"])):
         #     for p_val in self.table.page_directory["Base"][col_id]:
         #         print("{}".format(col_id))
@@ -23,20 +23,29 @@ class Query_Tester:
 
 
     def test_select(self):
-        for i in range(513):
+        for i in range(514):
             res = self.query.select(i, [1, 1, 1, 1, 1, 1, 1])
             print(i, res)
 
-        # for col_id in range(len(self.table.page_directory["Base"])):
-        #     for p_val in self.table.page_directory["Base"][col_id]:
-        #         print("{}".format(col_id))
-        #         print(p_val.data[0:100])
+    def test_update(self):
+        self.query.update(512, *[None, None, None, None, None, 1024])
+        #self.query.update(512, *[None, None, 1024, None, None, None])
+
+
+        for col_id in range(len(self.table.page_directory["Tail"])):
+            for p_val in self.table.page_directory["Tail"][col_id]:
+                print("{}".format(col_id))
+                print(p_val.data[0:100])
+        for col_id in range(len(self.table.page_directory["Base"])):
+            for p_val in self.table.page_directory["Base"][col_id]:
+                print("{}".format(col_id))
+                print(p_val.data[0:100])
 
 
     def run_all(self):
         self.test_insert()
         self.test_select()
-
+        self.test_update()
 
 def main():
     query_tester = Query_Tester()
