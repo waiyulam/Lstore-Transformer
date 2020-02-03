2e7673794f1a8d7304b67610d744ff4bc4c35e49
waiyulam <waiyu0616@outlook.com>
2020-02-02 17:52:52 -0800

    add test case for insert query

--- test.py
+++ test.py
@@ -9,17 +9,22 @@ class Query_Tester:
     def __init__(self):
         self.db = Database()
         self.table = self.db.create_table("test", 6, 0)
-
-    def test_insert(self):
         self.query = Query(self.table)
 
-        for i in range(514):
-            self.query.insert(i, 1+i, 2+i, 3+i, 4+i, 5+i)
-
-        # for col_id in range(len(self.table.page_directory["Base"])):
-        #     for p_val in self.table.page_directory["Base"][col_id]:
-        #         print("{}".format(col_id))
-        #         print(p_val.data[0:100])
+    def test_insert(self):
+        keys = []
+        for i in range(0, 513):
+            self.query.insert(906659671 + i, 93, 0, 0, 0, 0 )
+            keys.append(906659671 + i)
+            page_index,record_index = self.table.get(keys[i])
+            record = []
+            for j in range(len(self.table.page_directory["Base"])):
+                byte_value = self.table.page_directory["Base"][j][page_index].get(record_index)
+                if (j == 1):
+                    record.append(byte_value.decode()[4:])
+                else:
+                    record.append(int.from_bytes(byte_value, byteorder='big'))
+            print(record)
 
 
     def test_select(self):
@@ -57,8 +62,6 @@ class Query_Tester:
 
     def run_all(self):
         self.test_insert()
-        self.test_update()
-        self.test_select()
 
 def main():
     query_tester = Query_Tester()
