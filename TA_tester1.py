4562b4c9b08a20f15fb8999e34fbe0e9f9da4a20
waiyulam <waiyu0616@outlook.com>
2020-02-03 23:19:34 -0800

    merged

--- TA_tester1.py
+++ TA_tester1.py
@@ -1,9 +1,9 @@
-from template.page import Page
-from template.table import Table
 from template.query import Query
 from template.db import Database
 
 from random import choice, randint, sample, seed
+from colorama import Fore, Back, Style
+
 
 # Student Id and 4 grades
 db = Database()
@@ -31,7 +31,8 @@ for key in records:
     if error:
         print('select error on', key , ':', record, ', correct:', records[key])
     else:
-        print('select on', key, ':', record)
+        print('select on', key, ':', record.columns)
+
 for key in records:
     updated_columns = [None, None, None, None, None]
     for i in range(1, grades_table.num_columns):
@@ -48,19 +49,19 @@ for key in records:
         if error:
             print('update error on', original, 'and', updated_columns, ':', record, ', correct:', records[key])
         else:
-            print('update on', original, 'and', updated_columns, ':', record) 
+            print('update on', original, 'and', updated_columns, ':', record.columns) 
         updated_columns[i] = None
 
-# keys = sorted(list(records.keys()))
-# for c in range(0, grades_table.num_columns):
-#     for i in range(0, 20):
-#         r = sorted(sample(range(0, len(keys)), 2))
-#         column_sum = sum(map(lambda key: records[key][c], keys[r[0]: r[1] + 1]))
-#         result = query.sum(keys[r[0]], keys[r[1]], c)
-#         if column_sum != result:
-#             print('sum error on [', keys[r[0]], ',', keys[r[1]], ']: ', result, ', correct: ', column_sum)
-#         else:
-#             print('sum on [', keys[r[0]], ',', keys[r[1]], ']: ', column_sum)
+keys = sorted(list(records.keys()))
+for c in range(0, grades_table.num_columns):
+    for i in range(0, 20):
+        r = sorted(sample(range(0, len(keys)), 2))
+        column_sum = sum(map(lambda key: records[key][c], keys[r[0]: r[1] + 1]))
+        result = query.sum(keys[r[0]], keys[r[1]], c)
+        if column_sum != result:
+            print('sum error on [', keys[r[0]], ',', keys[r[1]], ']: ', result, ', correct: ', column_sum)
+        else:
+            print('sum on [', keys[r[0]], ',', keys[r[1]], ']: ', column_sum)
 
 
 
---
+++ TA_tester2.py
@@ -0,0 +1,58 @@
+from template.db import Database
+from template.query import Query
+from template.config import *
+
+from random import choice, randint, sample
+from colorama import Fore, Back, Style
+
+# Student Id and 4 grades
+db = Database()
+grades_table = db.create_table('Grades', 5, 0)
+query = Query(grades_table)
+
+records = {}
+
+for i in range(0, 1000):
+    key = 92106429 + randint(0, 9000)
+    while key in records:
+        key = 92106429 + randint(0, 9000)
+    records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
+    query.insert(*records[key])
+
+for key in records:
+    record = query.select(key, [1, 1, 1, 1, 1])[0]
+    for i, column in enumerate(record.columns):
+        if column != records[key][i]:
+            print(Fore.RED + 'Select error on key', key)
+            exit()
+print(Fore.GREEN + 'Passed SELECT test.')
+
+for key in records:
+    updated_columns = [None, None, None, None, None]
+    for i in range(1, grades_table.num_columns):
+        value = randint(0, 20)
+        updated_columns[i] = value
+        records[key][i] = value
+        record = query.select(key, [1, 1, 1, 1, 1])[0]
+        query.update(key, *updated_columns)
+        record = query.select(key, [1, 1, 1, 1, 1])[0]
+        for j, column in enumerate(record.columns):
+            if column != records[key][j]:
+                print(Fore.RED + 'Update error for key', key)
+                print(Fore.RED + 'Should have been', updated_columns)
+                print(Fore.RED + 'Returned result:', record)
+                exit()
+        updated_columns[i] = None
+print(Fore.GREEN + 'Passed UPDATE test.')
+
+# keys = sorted(list(records.keys()))
+# for c in range(0, grades_table.num_columns):
+#     for i in range(0, 20):
+#         r = sorted(sample(range(0, len(keys)), 2))
+#         column_sum = sum(map(lambda key: records[key][c], keys[r[0]: r[1] + 1]))
+#         result = query.sum(keys[r[0]], keys[r[1]], c)
+#         if column_sum != result:
+#             print(Fore.RED + 'Sum error for keys', keys[r[0]], 'to', keys[r[1]], 'on column', c)
+#             print(Fore.RED + 'Should have been', column_sum, ' but returned ', result)
+
+# print(Fore.GREEN + 'Passed SUM test.')
