5b07723a750a0d0fbee183c1e25d447f845e3248
Ye Wang <kyewang@ucdavis.edu>
2020-02-04 20:06:48 -0800

    add tests

--- TA_tester2.py
+++ TA_tester2.py
@@ -3,8 +3,10 @@ from template.query import Query
 from template.config import *
 
 from random import choice, randint, sample
-from colorama import Fore, Back, Style
-from time import time
+# from colorama import Fore, Back, Style
+from time import process_time
+import os
+os.system("clear")
 
 # Student Id and 4 grades
 db = Database()
@@ -13,26 +15,31 @@ query = Query(grades_table)
 
 records = {}
 
-start_time = time()
+# Measuring Insert Performance
+insert_time_0 = process_time()
 for i in range(0, 1000):
     key = 92106429 + randint(0, 9000)
     while key in records:
         key = 92106429 + randint(0, 9000)
     records[key] = [key, randint(0, 20), randint(0, 20), randint(0, 20), randint(0, 20)]
     query.insert(*records[key])
-print(Fore.GREEN + "Insert time: {}".format(time() - start_time))
+insert_time_1 = process_time()
+print("Inserting 1k records took:  \t\t\t", insert_time_1 - insert_time_0)
 
-start_time = time()
+# Measuring Select Performance
+select_time_0 = process_time()
 for key in records:
     record = query.select(key, [1, 1, 1, 1, 1])[0]
     for i, column in enumerate(record.columns):
         if column != records[key][i]:
             print(Fore.RED + 'Select error on key', key)
             exit()
-print(Fore.GREEN + 'Passed SELECT test.')
-print(Fore.GREEN + "Select time: {}".format(time() - start_time))
+# print(Fore.GREEN + 'Passed SELECT test.')
+select_time_1 = process_time()
+print("Selecting 1k records took:  \t\t\t", select_time_1 - select_time_0)
 
-start_time = time()
+# Measuring update Performance
+update_time_0 = process_time()
 for key in records:
     updated_columns = [None, None, None, None, None]
     for i in range(1, grades_table.num_columns):
@@ -49,10 +56,12 @@ for key in records:
                 print(Fore.RED + 'Returned result:', record)
                 exit()
         updated_columns[i] = None
-print(Fore.GREEN + 'Passed UPDATE test.')
-print(Fore.GREEN + "Select time: {}".format(time() - start_time))
+# print(Fore.GREEN + 'Passed UPDATE test.')
+update_time_1 = process_time()
+print("Updating 1k records took:  \t\t\t", update_time_1 - update_time_0)
 
-start_time = time()
+# Measuring Aggregate Performance
+agg_time_0 = process_time()
 keys = sorted(list(records.keys()))
 for c in range(0, grades_table.num_columns):
     for i in range(0, 20):
@@ -63,5 +72,13 @@ for c in range(0, grades_table.num_columns):
             print(Fore.RED + 'Sum error for keys', keys[r[0]], 'to', keys[r[1]], 'on column', c)
             print(Fore.RED + 'Should have been', column_sum, ' but returned ', result)
             exit()
-print(Fore.GREEN + 'Passed SUM test.')
-print(Fore.GREEN + "Sum time: {}".format(time() - start_time))
+# print(Fore.GREEN + 'Passed SUM test.')
+agg_time_1 = process_time()
+print("Aggregate 1k of 100 record batch took:\t", agg_time_1 - agg_time_0)
+
+# Measuring Delete Performance
+delete_time_0 = process_time()
+for i in range(0, 1000):
+    query.delete(906659671 + i)
+delete_time_1 = process_time()
+print("Deleting 1k records took:  \t\t\t", delete_time_1 - delete_time_0)
