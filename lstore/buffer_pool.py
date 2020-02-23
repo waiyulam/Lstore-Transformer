2f48828ad4d9d30e24f39510c4e07e211f471d49
zxcbvnmditto <chccheng@ucdavis.edu>
2020-02-23 13:36:45 -0800

    More Optimization for BufferPoo

--- lstore/buffer_pool.py
+++ lstore/buffer_pool.py
@@ -8,92 +8,85 @@ import time
 import copy
 
 
-class BufferPool():
+def read_page(page_path):
+    f = open(page_path, "rb")
+    page = pickle.load(f)  # Load entire page object
+    new_page = Page()
+    new_page.from_file(page)
+    f.close()
+    return new_page
 
+
+def write_page(page, page_path):
+    # Create if not existed
+    dirname = os.path.dirname(page_path)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+    f = open(page_path, "wb")
+    f.dump(page)  # Dump entire page object
+    f.close()
+
+
+class BufferPool():
     def __init__(self):
         self.size = BUFFER_POOL_SIZE
         self.path = None
 
         # active pages loaded in bufferpool
         self.page_directories = {}
-        self.tstamp_directories = {}
 
         # Pop the least freuqently used page
-        self.timestamp = []  # [[timestamp1, uid1], [timestamp2, uid2] ...]
+        self.tstamp_directories = {}
 
     def initial_path(self, path):
         self.path = path
 
     def add_meta(self, uid):
         self.page_directories[uid] = None
-        self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
 
-    def read_page(self, page_path):
-        f = open(page_path, "rb")
-        page = pickle.load(f)  # Load entire page object
-        new_page = Page()
-        new_page.from_file(page)
-        f.close()
-        return new_page
+    def is_full(self):
+        return len(self.tstamp_directories) >= self.size
 
-    def write_page(self, page, page_path):
-        # Create if not existed
-        dirname = os.path.dirname(page_path)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        f = open(page_path, "wb")
-        f.dump(page)  # Dump entire page object
-        f.close()
+    def is_page_in_buffer(self, uid):
+        return self.page_directories[uid] is None
+
+    def uid_to_path(self, uid):
+        """
+        Convert uid to path
+        uid: tuple(table_name, base_tail, column_id, page_range_id, page_id)
+        """
+        t_name, base_tail, column_id, page_range_id, page_id = uid
+        path = os.path.join(self.path, t_name, base_tail, column_id,
+                            page_range_id, str(page_id) + ".pkl")
+        return path
 
     def get_page(self, t_name, base_tail, column_id, page_range_id, page_id):
         uid = tuple(t_name, base_tail, column_id, page_range_id, page_id)
-        page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
-        page = self.page_directories(uid)
+        page_path = self.uid_to_path(uid)
 
         # Page not loaded in buffer, load from disk
-        if page is None:
-            # Have Space in bufferbool
-            if not self.is_full():
-                self.page_directories[uid] = self.read_page(page_path)
-                # self.timestamp.append([datetime.timestamp(datetime.now()), uid])
-                self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
-
-            # No Space in bufferbool
-            else:
+        if self.is_page_in_buffer(uid):
+            # No Space in bufferbool, write LRU page to disk
+            if self.is_full():
                 # Pop least recently used page in cache
-                # sorted_timestamps = sorted(self.timestamp)
-                # oldest_freq, oldest_uid = sorted_timestamps[0] # FIXME: More complex control needed for pinning
-                sorted_uids = sorted(self.tstamp_directories, key=self.tstamp_directories.get)
-                oldest_uid = sorted_uids # FIXME: More complex control needed for pinning
+                sorted_uids = sorted(self.tstamp_directories,
+                                     key=self.tstamp_directories.get)
+                oldest_uid = sorted_uids[0]  # FIXME: More complex control needed for pinning
                 oldest_page = self.page_directories[oldest_uid]
                 assert(oldest_page is not None)
 
                 # Check if old_page is dirty => write back
                 if oldest_page.dirty == 1:
-                    t_name, base_tail, column_id, page_range_id, page_id = oldest_uid
-                    old_page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
-                    self.write_page(oldest_page, old_page_path)
+                    old_page_path = self.uid_to_path(oldest_uid)
+                    write_page(oldest_page, old_page_path)
 
-                # Load target page
-                new_page = self.read_page(page_path)
-
-                # Reset States
                 self.page_directories[oldest_uid] = None
-                self.page_directories[uid] = new_page
                 del self.tstamp_directories[oldest_uid]
-                self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
-                # self.timestamp[oldest_page_id] = [datetime.timestamp(datetime.now()), uid]
-            page = new_page
-        # Page already in buffer
-        else:
-            self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
-            # self.timestamp[page_id][0] = datetime.timestamp(datetime.now())  # Update timestamp
 
-        return page
+            self.page_directories[uid] = read_page(page_path)
 
-    def is_full(self):
-        # return len(self.timestamp) >= self.size
-        return len(self.tstamp_directories) >= self.size
+        self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
+        return self.page_directories[uid]
 
     def close(self):
         active_uids = self.tstamp_directories.values()
@@ -104,12 +97,9 @@ class BufferPool():
                 page = self.page_directories[uid]
                 # Write Back Dirty Pages
                 if page.dirty and not page.pinned:
-                    # _, uid = self.timestamp[page_id]
-                    t_name, base_tail, column_id, page_range_id, page_id = uid
-                    page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
-                    self.write_page(page, page_path)
+                    page_path = self.uid_to_path(uid)
+                    write_page(page, page_path)
                     active_uids.pop(uid)
-                    # self.pages.pop(page_id)
 
             # Wait until Pinned Pages are unpinned
             time.sleep(1)
