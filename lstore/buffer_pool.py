ab20a18d1b172938b5043e9e91731bba8708405b
zxcbvnmditto <chccheng@ucdavis.edu>
2020-02-23 13:10:35 -0800

    Small Modification on BufferPool

--- lstore/buffer_pool.py
+++ lstore/buffer_pool.py
@@ -15,10 +15,8 @@ class BufferPool():
         self.path = None
 
         # active pages loaded in bufferpool
-        self.pages = []
-
-        # A look up table if a page is being used of not
-        self.uid_2_pageid = {}  # key: uid, value: page_id (index of self.pages)
+        self.page_directories = {}
+        self.tstamp_directories = {}
 
         # Pop the least freuqently used page
         self.timestamp = []  # [[timestamp1, uid1], [timestamp2, uid2] ...]
@@ -27,8 +25,8 @@ class BufferPool():
         self.path = path
 
     def add_meta(self, uid):
-        if uid not in self.uid_2_pageid.keys():  # TODO: This might be unnecessary
-            self.uid_2_pageid[uid] = -1
+        self.page_directories[uid] = None
+        self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
 
     def read_page(self, page_path):
         f = open(page_path, "rb")
@@ -50,64 +48,68 @@ class BufferPool():
     def get_page(self, t_name, base_tail, column_id, page_range_id, page_id):
         uid = tuple(t_name, base_tail, column_id, page_range_id, page_id)
         page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
-        page_id = self.uid_2_pageid[uid]
+        page = self.page_directories(uid)
 
         # Page not loaded in buffer, load from disk
-        if page_id == -1:
+        if page is None:
             # Have Space in bufferbool
             if not self.is_full():
-                new_page = self.read_page(page_path)
-                page_id = len(self.pages)
-                self.uid_2_pageid[uid] = page_id
-                self.pages.append(new_page)
-                self.timestamp.append([datetime.timestamp(datetime.now()), uid])
+                self.page_directories[uid] = self.read_page(page_path)
+                # self.timestamp.append([datetime.timestamp(datetime.now()), uid])
+                self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
 
             # No Space in bufferbool
             else:
                 # Pop least recently used page in cache
-                sorted_timestamps = sorted(self.timestamp)
-                oldest_freq, oldest_uid = sorted_timestamps[0] # FIXME: More complex control needed for pinning
-                oldest_page_id = self.uid_2_pageid[oldest_uid]
-                assert(oldest_page_id != -1)
-                old_page = self.pages[oldest_page_id]
+                # sorted_timestamps = sorted(self.timestamp)
+                # oldest_freq, oldest_uid = sorted_timestamps[0] # FIXME: More complex control needed for pinning
+                sorted_uids = sorted(self.tstamp_directories, key=self.tstamp_directories.get)
+                oldest_uid = sorted_uids # FIXME: More complex control needed for pinning
+                oldest_page = self.page_directories[oldest_uid]
+                assert(oldest_page is not None)
 
                 # Check if old_page is dirty => write back
-                if old_page.dirty == 1:
+                if oldest_page.dirty == 1:
                     t_name, base_tail, column_id, page_range_id, page_id = oldest_uid
                     old_page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
-                    self.write_page(old_page, old_page_path)
+                    self.write_page(oldest_page, old_page_path)
 
                 # Load target page
                 new_page = self.read_page(page_path)
 
                 # Reset States
-                self.uid_2_pageid[oldest_uid] = -1
-                self.uid_2_pageid[uid] = oldest_page_id
-                self.pages[oldest_page_id] = new_page
-                self.timestamp[oldest_page_id] = [datetime.timestamp(datetime.now()), uid]
+                self.page_directories[oldest_uid] = None
+                self.page_directories[uid] = new_page
+                del self.tstamp_directories[oldest_uid]
+                self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
+                # self.timestamp[oldest_page_id] = [datetime.timestamp(datetime.now()), uid]
             page = new_page
-        # Page already in buffer, load from disk
+        # Page already in buffer
         else:
-            page_id = self.uid_2_pageid[uid]
-            self.timestamp[page_id][0] = datetime.timestamp(datetime.now()) # Update timestamp
-            page = self.pages[page_id]
+            self.tstamp_directories[uid] = datetime.timestamp(datetime.now())
+            # self.timestamp[page_id][0] = datetime.timestamp(datetime.now())  # Update timestamp
 
         return page
 
     def is_full(self):
-        return len(self.pages) >= self.size
+        # return len(self.timestamp) >= self.size
+        return len(self.tstamp_directories) >= self.size
 
     def close(self):
-        while len(self.pages) > 0:
-            pages = copy.deepcopy(self.pages)
+        active_uids = self.tstamp_directories.values()
+        while len(active_uids) > 0:
+            active_uids_copy = copy.deepcopy(active_uids)
             # Loop Through Pages in Bufferpool
-            for page_id, page in enumerate(pages):
+            for i, uid in enumerate(active_uids_copy):
+                page = self.page_directories[uid]
                 # Write Back Dirty Pages
                 if page.dirty and not page.pinned:
-                    _, uid = self.timestamp[page_id]
+                    # _, uid = self.timestamp[page_id]
                     t_name, base_tail, column_id, page_range_id, page_id = uid
                     page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
                     self.write_page(page, page_path)
-                    self.pages.pop(page_id)
+                    active_uids.pop(uid)
+                    # self.pages.pop(page_id)
+
             # Wait until Pinned Pages are unpinned
             time.sleep(1)
