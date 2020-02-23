dfc1fab5ee8433c0e8abf97d0961118a61c72222
zxcbvnmditto <chccheng@ucdavis.edu>
2020-02-23 00:48:45 -0800

    Fix syntax

--- lstore/buffer_pool.py
+++ lstore/buffer_pool.py
@@ -7,31 +7,27 @@ from datetime import datetime
 import time
 import copy
 
+
 class BufferPool():
 
     def __init__(self, path):
         self.size = BUFFER_POOL_SIZE
-        
         self.path = None
 
         # active pages loaded in bufferpool
         self.pages = []
-        # self.is_dirty = []
-        # self.is_pinned = []
 
         # A look up table if a page is being used of not
-        self.uid_2_pageid = {} # key: uid, value: page_id (index of self.pages)
+        self.uid_2_pageid = {}  # key: uid, value: page_id (index of self.pages)
 
         # Pop the least freuqently used page
-        self.timestamp = [] # [[timestamp1, uid1], [timestamp2, uid2] ...]
+        self.timestamp = []  # [[timestamp1, uid1], [timestamp2, uid2] ...]
 
-    
     def init_path(self, path):
         self.path = path
 
-    
     def initial_meta(self, meta_f, t_name):
-        f = open(meta_f, "r") as f:
+        f = open(meta_f, "r")
         lines = f.readlines()
         f.close()
 
@@ -40,7 +36,7 @@ class BufferPool():
             uid = tuple(t_name, base_tail, column_id, page_range_id, page_id)
             if uid not in self.uid_2_pageid.keys():
                 self.uid_2_pageid[uid] = -1
-        
+
     # def initial_pool(self):
     #     # Set used Tables
     #     self.used_frequency = heapq.nlargest(self.size, self.all_frequency)
@@ -51,36 +47,32 @@ class BufferPool():
     #         # Point to corresponding page index
     #         self.uid_2_pageid[uid] = len(self.pages)
     #         self.pages.append(page)
-    
 
     def read_page(self, page_path):
         f = open(page_path, "rb")
-        page = pickle.load(f) # Load entire page object
+        page = pickle.load(f)  # Load entire page object
         new_page = Page()
         new_page.from_file(page)
         f.close()
         return new_page
-
 
     def write_page(self, page, page_path):
         # Create if not existed
         dirname = os.path.dirname(page_path)
         if not os.path.exists(dirname):
             os.makedirs(dirname)
-            
         f = open(page_path, "wb")
-        f.dump(page) # Dump entire page object
+        f.dump(page)  # Dump entire page object
         f.close()
-
 
     def get_page(self, t_name, base_tail, column_id, page_range_id, page_id):
         uid = tuple(t_name, base_tail, column_id, page_range_id, page_id)
         page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
         page_id = self.uid_2_pageid[uid]
-        
+
         # Page not loaded in buffer, load from disk
         if page_id == -1:
-            # Have Space in bufferbool 
+            # Have Space in bufferbool
             if not self.is_full():
                 new_page = self.read_page(page_path)
                 page_id = len(self.pages)
@@ -123,10 +115,9 @@ class BufferPool():
     def is_full(self):
         return len(self.pages) >= self.size
 
-
     def write_back_all(self):
         while len(self.pages) > 0:
-            pages = copy.deepcopy(self.pages)            
+            pages = copy.deepcopy(self.pages)
             # Loop Through Pages in Bufferpool
             for page_id, page in enumerate(pages):
                 # Write Back Dirty Pages
@@ -136,7 +127,5 @@ class BufferPool():
                     page_path = os.path.join(self.path, t_name, base_tail, column_id, page_range_id, str(page_id) + ".txt")
                     self.write_page(page, page_path)
                     self.pages.pop(page_id)
-                    
             # Wait until Pinned Pages are unpinned
             time.sleep(1)
-
